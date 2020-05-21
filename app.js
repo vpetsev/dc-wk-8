6a464ebac9d0721a5e3ed728a6081768ca269cc0
Victor Petsev <59983319+VPetsev@users.noreply.github.com>
2020-05-21 15:58:48 -0500

    full-stack assignment with multiple database tables

--- app.js
+++ app.js
@@ -5,22 +5,24 @@ const models = require('./models')
 
 app.use(express.urlencoded())
 
+// tell express to use mustache templating engine
 app.engine('mustache', mustacheExpress())
+// the pages are located in views directory
 app.set('views', './views')
+// extension will be .mustache
 app.set('view engine', 'mustache')
 
-
-app.get('/posts', (req, res)=>{
+app.get("/posts", (req, res) => {
     models.Post.findAll().then(posts => {
-        res.render('index', {posts: posts})
+        res.render("index", {posts: posts})
     })
 })
 
-app.get('/add-post', (req, res) => {
-    res.render('add-post')
+app.get("/add-post", (req, res) => {
+    res.render("add-post")
 })
 
-app.post('/add-post', (req, res)=>{
+app.post("/add-post", (req, res) => {
     let title = req.body.postTitle
     let category = req.body.postCategory
     let body = req.body.postBody
@@ -36,67 +38,92 @@ app.post('/add-post', (req, res)=>{
     })
 })
 
-app.post("/delete-post", (req, res) =>{
+app.post("/delete-post", (req, res) => {
     let postId = req.body.postId
-    console.log("postid is " + postId)
-
     models.Post.destroy({
         where: {
             id: postId
         }
-    }).then(deletedPost => { 
-        console.log(deletedPost)
+    }).then(deletedPost => {
         res.redirect("posts")
     })
 })
 
-app.post("/update-post", (req, res) => {
-    let title = req.body.postTitle
-    let category = req.body.postCategory
-    let body = req.body.postBody
-    let postId = req.body.postId
-    models.Post.update({
-        title: title,
-        category: category,
-        body: body
-     }, { 
-        where: {
-            id: postId
-     }
-    }).then(post => {
-        console.log(post + " updated")
-        res.redirect("posts")
-    }) 
-})
-
-app.get("/post-details/:postId", (req, res) =>{
+app.get("/post-details/:postId", (req, res) => {
     let postId = req.params.postId
-    models.Post.findByPk(postId)
-    .then(updatedPost => {
-        console.log("=====", updatedPost.dataValues)
-        // res.render("post-details", {
-        //     title: updatedPost.title,
-        //     category: updatedPost.category,
-        //     body: updatedPost.body,
-        //     postId: updatedPost.id
-        // })
-        res.render("post-details", updatedPost.dataValues)
+    models.Post.findByPk(postId, {
+        include: [
+            {
+                model: models.Comment,
+                as: 'comments'
+            }
+        ]
+    }).then(post => {
+            res.render("post-details", post.dataValues)
     })
 })
 
-app.post("/filter-posts", (req, res)=>{
+app.get("/comments/:commentId", (req, res)=>{ 
+    let commentId = req.params.commentId
+
+    models.Comment.findByPk(commentId, {
+        include: [
+            {
+                model: models.Post,
+                as: "post"
+            }
+        ]
+    }).then(comment => {
+        console.log(comment)
+        res.redirect(`/posts/${postId}`)
+    })
+})
+
+app.post("/add-comment", (req, res) => {
+    let title = req.body.title
+    let body = req.body.body
+    let postId = req.body.postId
+
+    let comment = models.Comment.build({
+        title: title,
+        body: body,
+        post_id: postId
+    })
+
+    review.save().then(savedReview => {
+        res.redirect(`/posts/${postId}`)
+    })
+})
+
+// app.post("/update-post", (req, res) => {
+//     let title = req.body.postTitle
+//     let category = req.body.postCategory
+//     let body = req.body.postBody
+//     let postId = req.body.postId
+//     models.Post.update({
+//         title: title,
+//         category: category,
+//         body: body
+//     }, {
+//             where: {
+//             id: postId
+//         }
+//     }).then(post => {
+//         res.redirect("posts")
+//     })
+// })
+
+app.post("/filter-posts", (req, res) => {
     let filterCategory = req.body.postCategory
-    console.log(filterCategory)
     models.Post.findAll({
         where: {
             category: filterCategory
         }
     }).then(filteredPosts => {
-        console.log(filteredPosts)
         res.render("filter", {filteredPosts: filteredPosts})
     })
 })
 
 app.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000")
+    console.log("server is being hosted on http://localhost:3000")
 })
